ef09eb177c441808d441b7581761e1341cdd4248
PythonSaga <156877353+PythonSaga@users.noreply.github.com>
2024-02-12 17:08:45 +0530

    Update data.py

--- Benchmarking/data.py
+++ Benchmarking/data.py
@@ -5,7 +5,7 @@ import os
 
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
-Python_Saga = os.path.join(ROOT, "..", "Dataset", "sample_input.jsonl.gz") # this is the file that contains the tasks (prompts) and the test suites (test_suite) and the test suite solutions (test_suite_solutions) for each task
+Python_Saga = os.path.join(ROOT, "..", "DataSet", "sample_input.jsonl.gz") # this is the file that contains the tasks (prompts) and the test suites (test_suite) and the test suite solutions (test_suite_solutions) for each task
 
 
 def read_problems(evalset_file: str = Python_Saga) -> Dict[str, Dict]: # Dict[str, Dict[str, str]] means a dictionary with keys: task_id and values: another dictionary with keys: prompt, test_suite and test_suite_solutions (all strings)
